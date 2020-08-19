7bbc7c44a15827016f6d8e933089953c46adc662
Ricardo <ricardocavalcantesilva@gmail.com>
2020-08-18 22:36:27 -0300

    Desenvolvimento das rotas GET, POST, PUT, DEL

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +11,80 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.status(200).json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+
+  repositories.push(repository);
+
+  return response.status(201).json(repository);
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const index = repositories.findIndex(repository => repository.id == id);
+
+  if (index < 0) {
+    return response.status(400).json({ message: "Repository is not exists." });    
+  }
+
+  const repository = {
+    id,
+    title, 
+    url, 
+    techs,
+    likes: repositories[index].likes
+  } 
+
+  repositories[index] = repository;
+
+  return response.status(200).json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const index = repositories.findIndex(repository => repository.id == id);
+
+  if (index < 0) {
+    return response.status(400).json({ message: "Repository is not exists." });    
+  }
+    
+  repositories.splice(index, 1);
+
+  return response.status(204).send();
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
-});
+  const { id } = request.params;
+
+  const index = repositories.findIndex(repository => repository.id == id);
+
+  if (index < 0) {
+    return response.status(400).json({ message: "Repository is not exists." });    
+  }
+
+  const qtdLike = repositories[index].likes;
+
+  repositories[index].likes = qtdLike + 1;
+
+  const repository = repositories[index];
+
+  return response.status(200).json(repository);
+
+  });
 
 module.exports = app;
